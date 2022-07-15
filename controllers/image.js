e50212044edaf5f3e1aa8a45c07593b27dae3a77
Robin Punnoose <robin.punnoose@protonmail.com>
2022-07-15 17:37:33 -0500

    added clarifai

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: "8c5421a9efcb40009093494ccc523fc7",
+  })
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}  
+
 const handleImage = (req,res, knex) => {
     const {id} = req.body
     
@@ -11,5 +26,6 @@ const handleImage = (req,res, knex) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage,
+    handleApiCall
 }
--- server.js
+++ server.js
@@ -27,17 +27,14 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req,res)=> {
-    // res.send(database.users)
+app.get('/', (req,res)=> { 
+  // res.send(knex.users) 
 })
-
 app.post('/signin', (req,res) => {signin.handleSignin(req, res, knex, bcrypt)})
-
 app.post('/register', (req,res) => {register.handleRegister(req, res, knex, bcrypt)})
-
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, knex)})  
-
 app.put('/image', (req,res) => {image.handleImage(req, res, knex)})
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
     console.log('app is running')
