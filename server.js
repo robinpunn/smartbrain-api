f887b49913545e55291c410c36845609c730c101
Robin Punnoose <robin.punnoose@protonmail.com>
2022-07-16 18:44:28 -0500

    connecting to heroku database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-round-78251',
     user : 'ubuntu',
     port: 5432,
     password : 'toor',
