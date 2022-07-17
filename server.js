7f21fc49dcdaa830026301da62b75014e3666861
Robin Punnoose <robin.punnoose@protonmail.com>
2022-07-16 20:44:00 -0500

    server.js now using database on heroku

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-round-78251',
-    user : 'ubuntu',
-    port: 5432,
-    password : 'toor',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
